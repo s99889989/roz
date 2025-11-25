64fbffea3cfa7c4eb0f82cde584fe5bae6e44d61
s99889989 <s02048898067@gmail.com>
2025-11-26 00:47:32 +0800

    2025/11/25-23:15 修正魔物地圖搜尋問題。

--- app/pages/navbar.vue
+++ app/pages/navbar.vue
@@ -18,7 +18,7 @@
         <ul  style="background-color: #606753" class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 
           <li>
-            <NuxtLink to="/monster/monsterViewer" href="#" class="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+            <NuxtLink to="/monster/monster" href="#" class="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
               魔物查詢</NuxtLink>
           </li>
 
